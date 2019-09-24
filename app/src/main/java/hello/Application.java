53ca73d58b55128d6d8af37605478a1413f7452f
JeroenPeters96 <jeroen.peters1@hotmail.nl>
2019-09-24 10:45:35 +0200

    usefull keepo

--- app/src/main/java/hello/Application.java
+++ app/src/main/java/hello/Application.java
@@ -20,7 +20,7 @@ public class Application {
         return args -> {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-
+            //nicememe
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
